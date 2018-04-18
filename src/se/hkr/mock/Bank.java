94f250b75800cdae220d4d34c462facc63fb9e04
Simon Waidringer <simonludvigsjunk@gmail.com>
2018-04-18 14:11:22 +0200

    Updated the ATM class accroding to UML

---
+++ src/se/hkr/mock/Bank.java
@@ -0,0 +1,14 @@
+package se.hkr.mock;
+
+/**
+ * Created by Simon Waidringer on 2018-04-18.
+ */
+public class Bank {
+
+    public Bank() {
+    }
+
+    public double getBalance(int UserID){
+        return 77.77;
+    }
+}
