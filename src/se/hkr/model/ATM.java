0a3c88da1285004694f0a82fab1cdfcd00b64545
Simon Waidringer <simonludvigsjunk@gmail.com>
2018-04-18 10:40:22 +0200

    Second update

--- src/se/hkr/model/ATM.java
+++ src/se/hkr/model/ATM.java
@@ -11,7 +11,7 @@ public class ATM extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("ATM");
+        primaryStage.setTitle("ATM-ATM");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
